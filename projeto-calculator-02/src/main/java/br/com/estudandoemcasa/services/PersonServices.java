f46763e5070accf4a6e36dac12b527f512012eb7
Miguel Pereira da Silva <miguel.silva@avanade.com>
2022-03-29 10:49:50 -0300

    Retornando Listas de Objetos

--- projeto-calculator-02/src/main/java/br/com/estudandoemcasa/services/PersonServices.java
+++ projeto-calculator-02/src/main/java/br/com/estudandoemcasa/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.estudandoemcasa.services;
 import br.com.estudandoemcasa.entity.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
@@ -20,6 +22,27 @@ public class PersonServices {
         person.setFirstName("Pereira da Silva");
         person.setAdress("Taboão da Serra - SP");
         person.setGender("Male");
+
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Orange " + i);
+        person.setLastName("Last Name " + i);
+        person.setAdress("Adress " + i);
+        person.setGender("Gender " + i);
 
         return person;
     }
