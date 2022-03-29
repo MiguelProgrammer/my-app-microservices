2f1c583145b65b81c23e85a4880844fed5429e3e
Miguel Pereira da Silva <miguel.silva@avanade.com>
2022-03-29 13:09:39 -0300

    Os Verbos POST, PUT e DELETE

--- projeto-calculator-02/src/main/java/br/com/estudandoemcasa/controller/PersonController.java
+++ projeto-calculator-02/src/main/java/br/com/estudandoemcasa/controller/PersonController.java
@@ -5,10 +5,7 @@ import br.com.estudandoemcasa.entity.Person;
 import br.com.estudandoemcasa.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,18 +13,32 @@ import java.util.List;
 @RequestMapping("/person")
 public class PersonController {
 
-	@Autowired
-	private PersonServices personServices;
+    @Autowired
+    private PersonServices personServices;
 
 
-	@RequestMapping(value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
-		return personServices.findAll();
-	}
+    @RequestMapping(value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return personServices.findAll();
+    }
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable("id") String id) {
-		return personServices.findById(id);
-	}
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") String id) {
+        return personServices.findById(id);
+    }
 
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return personServices.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return personServices.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        personServices.delete(id);
+    }
 }
